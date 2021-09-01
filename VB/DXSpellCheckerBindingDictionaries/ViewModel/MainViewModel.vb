777e37aa460f047851fd1c0df394fd09c4e1600f
DevExpressExampleBot <fakeEmail>
2021-09-01 18:22:36 +0300

    Source auto update [skip ci]

--- VB/DXSpellCheckerBindingDictionaries/ViewModel/MainViewModel.vb
+++ VB/DXSpellCheckerBindingDictionaries/ViewModel/MainViewModel.vb
@@ -5,24 +5,24 @@ Imports System.Globalization
 Imports DevExpress.Mvvm.DataAnnotations
 
 Namespace DXSpellCheckerBindingDictionaries.ViewModel
-    #Region "#ViewModel"
-    Public Class MainViewModel
-        Public Overridable Property Dictionaries() As DictionarySourceCollection
-        Public Sub New()
-            Dictionaries = GetDictionaries()
-        End Sub
+	#Region "#ViewModel"
+	Public Class MainViewModel
+		Public Overridable Property Dictionaries() As DictionarySourceCollection
+		Public Sub New()
+			Dictionaries = GetDictionaries()
+		End Sub
 
-        Public Function GetDictionaries() As DictionarySourceCollection
-            Dim collection = New DictionarySourceCollection()
+		Public Function GetDictionaries() As DictionarySourceCollection
+			Dim collection = New DictionarySourceCollection()
 
-            Dim dictionary = New HunspellDictionarySource()
-            dictionary.Culture = New CultureInfo("de-DE")
-            dictionary.DictionaryUri = New Uri("pack://application:,,,/Dictionaries/de_DE.dic")
-            dictionary.GrammarUri = New Uri("pack://application:,,,/Dictionaries/de_DE.aff")
-            collection.Add(dictionary)
-            Return collection
-        End Function
-    End Class
-    #End Region ' #ViewModel
+			Dim dictionary = New HunspellDictionarySource()
+			dictionary.Culture = New CultureInfo("de-DE")
+			dictionary.DictionaryUri = New Uri("pack://application:,,,/Dictionaries/de_DE.dic")
+			dictionary.GrammarUri = New Uri("pack://application:,,,/Dictionaries/de_DE.aff")
+			collection.Add(dictionary)
+			Return collection
+		End Function
+	End Class
+	#End Region ' #ViewModel
 End Namespace
 
