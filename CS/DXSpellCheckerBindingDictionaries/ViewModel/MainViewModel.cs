915ff2eb283dbe5066d61c923c5126a469b9da22
AnnaVekhina <42933497+AnnaVekhina@users.noreply.github.com>
2022-01-14 16:42:52 +0300

    Update MainViewModel.cs

--- CS/DXSpellCheckerBindingDictionaries/ViewModel/MainViewModel.cs
+++ CS/DXSpellCheckerBindingDictionaries/ViewModel/MainViewModel.cs
@@ -10,9 +10,15 @@ namespace DXSpellCheckerBindingDictionaries.ViewModel
     public class MainViewModel
     {
         public virtual DictionarySourceCollection Dictionaries { get; set; }
+        public virtual SpellChecker SpellChecker { get; set; }
+
         public MainViewModel()
         {
             Dictionaries = GetDictionaries();
+            SpellChecker = new SpellChecker();
+            SpellChecker.SpellingFormType = DevExpress.XtraSpellChecker.SpellingFormType.Word;
+            SpellChecker.SpellCheckMode = DevExpress.XtraSpellChecker.SpellCheckMode.AsYouType;
+            SpellChecker.Culture = new CultureInfo("de-DE");
         }
 
         public DictionarySourceCollection GetDictionaries()
@@ -21,8 +27,8 @@ namespace DXSpellCheckerBindingDictionaries.ViewModel
 
             var dictionary = new HunspellDictionarySource();
             dictionary.Culture = new CultureInfo("de-DE");            
-            dictionary.DictionaryUri = new Uri(@"pack://application:,,,/Dictionaries/de_DE.dic");
-            dictionary.GrammarUri = new Uri(@"pack://application:,,,/Dictionaries/de_DE.aff");
+            dictionary.DictionaryUri = new Uri(@"pack://application:,,,/DXSpellCheckerBindingDictionaries;component//Dictionaries/de_DE.dic");
+            dictionary.GrammarUri = new Uri(@"pack://application:,,,/DXSpellCheckerBindingDictionaries;component//Dictionaries/de_DE.aff");
             collection.Add(dictionary);
             return collection;
         }
