12f2d96c157aff452fd4b4e6121a1aecb19c83c8
DevExpressExampleBot <fakeEmail>
2022-01-18 10:56:15 +0300

    Source auto update [skip ci]

--- VB/DXSpellCheckerBindingDictionaries/ViewModel/MainViewModel.vb
+++ VB/DXSpellCheckerBindingDictionaries/ViewModel/MainViewModel.vb
@@ -1,7 +1,10 @@
 Imports System
-Imports DevExpress.Xpf.SpellChecker
 Imports System.Globalization
+#Region "#usings"
+Imports DevExpress.Xpf.SpellChecker
+Imports DevExpress.XtraSpellChecker
 
+#End Region  ' #usings
 Namespace DXSpellCheckerBindingDictionaries.ViewModel
 
 #Region "#ViewModel"
@@ -12,19 +15,28 @@ Namespace DXSpellCheckerBindingDictionaries.ViewModel
         Public Overridable Property SpellChecker As SpellChecker
 
         Public Sub New()
-            Dictionaries = GetDictionaries()
+            ' Create a SpellChecker instance.
             SpellChecker = New SpellChecker()
-            SpellChecker.SpellingFormType = DevExpress.XtraSpellChecker.SpellingFormType.Word
-            SpellChecker.SpellCheckMode = DevExpress.XtraSpellChecker.SpellCheckMode.AsYouType
+            ' Specify the SpellChecker's proterties.
+            SpellChecker.SpellingFormType = SpellingFormType.Word
+            SpellChecker.SpellCheckMode = SpellCheckMode.AsYouType
             SpellChecker.Culture = New CultureInfo("de-DE")
+            ' Obtain a dictionary collection.
+            Dictionaries = GetDictionaries()
         End Sub
 
         Public Function GetDictionaries() As DictionarySourceCollection
+            ' Create a dictionary collection.
             Dim collection = New DictionarySourceCollection()
+            ' Create a Hunspell spell-checking dictionary.
             Dim dictionary = New HunspellDictionarySource()
+            ' Specify the dictionary culture settings.
             dictionary.Culture = New CultureInfo("de-DE")
+            ' Load the dictionary file.
             dictionary.DictionaryUri = New Uri("pack://application:,,,/DXSpellCheckerBindingDictionaries;component/Dictionaries/de_DE.dic")
+            ' Load the affix file.
             dictionary.GrammarUri = New Uri("pack://application:,,,/DXSpellCheckerBindingDictionaries;component/Dictionaries/de_DE.aff")
+            ' Add the dictionary to the collection.
             collection.Add(dictionary)
             Return collection
         End Function
